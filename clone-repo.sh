4137a24957b39ecaf5cb07597abecf98d0abb1be
Oracle Public Cloud User <opc@rackbeat-tolgee.subnet09181620.vcn09181620.oraclevcn.com>
2025-03-24 14:24:00 +0000

    Add logic for private repo

---
+++ clone-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "Cloning repo ⏬"
+
+# Check if GIT_USER and GIT_PASS are set
+if [ -n "$GIT_USER" ] && [ -n "$GIT_PASS" ]; then
+    echo "Using authenticated Git clone 🫡"
+    REPO_PATH=$(echo ${REPO_URL} | sed 's/https:\/\/github.com\///')
+    git clone https://${GIT_USER}:${GIT_PASS}@github.com/${REPO_PATH} app
+else
+    echo "Cloning anonymously 😶‍🌫️ "
+    git clone ${REPO_URL} app
+fi
